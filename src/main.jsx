924c59b620b665334c4dc9f0aefc686e9a238b80
cchampou <clement@champouillon.com>
2021-11-16 06:02:39 +0100

    chore: adding stylesheets preloading to the game

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import MyInput from "./useId";
-import Title from "./components/title/Title";
 
-const Application = ({ script }) => {
+const Title = lazy(() => import('./components/title/Title.jsx'));
+
+const Application = ({ script, styleSheets }) => {
 
   return (
     <html lang="en">
     <head>
       <title>React 18</title>
       <meta charSet="UTF-8"/>
+      {Array.isArray(styleSheets) && styleSheets.map(sheet => <link rel="stylesheet" href={sheet} />)}
     </head>
     <body>
     <div id="app">
-      <Title>Hello world !</Title>
-      <MyInput />
+      <Suspense fallback={<div />}>
+        <Title>Hello world !</Title>
+        <MyInput />
+      </Suspense>
     </div>
     <script type="module" src={script}></script>
     </body>
